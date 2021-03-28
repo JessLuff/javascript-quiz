49208ded0a3779b55fe3723919c502bd5b27c583
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-03-29 01:49:57 +1100

    Quiz start, timer and buttons updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+    
     var question = [
         {
             title: "Question 1",
@@ -18,15 +20,66 @@
         }
     ];
 
-    var head = document.createElement("h1");
-    head.textContent = "Javascript Quiz";
-    document.body.appendChild(head);
-    var text = document.createElement("p");
-    text.textContent = "Answer questions on Javascript fundamentals as quickly and accurately as possible. You have 60 seconds, for each incorrect answer a 10 second penalty will be applied. Click Start to begin";
-    document.body.appendChild(text);
+    var secondsLeft = 60;
 
-    //var timeEl = document.querySelector(".time");
-    // when times runs out changes head, body all?
+    function setTime() {
+        // Sets interval in variable
+        var timerInterval = setInterval(function() {
+          secondsLeft--;
+          timeEl.textContent = secondsLeft + " seconds";
+
+          if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            timeEl.textContent = "Times up";
+          }
+      
+        }, 1000);  
+    }
+
+    function startQuiz(event){
+        setTime();
+        headEl.textContent = "Quiz starting";
+        textEl.textContent = "First question coming up";
+        start.style.display = "none";
+        b1.style.display = "block";
+        b2.style.display = "block";
+        b3.style.display = "block";
+        b4.style.display = "block";
+
+        runQuiz();
+    }
+
+    function runQuiz(){
+
+
+        b1.addEventListener("click", startQuiz);
+        b2.addEventListener("click", startQuiz);
+        b3.addEventListener("click", startQuiz);
+        b4.addEventListener("click", startQuiz);
+
+        return;
+    }
+
+
+    var timeEl = document.querySelector(".time");
+    timeEl.textContent = "Start timer";
+
+    var headEl = document.querySelector(".head");
+    headEl.textContent = "Javascript Quiz";
+
+    var textEl = document.querySelector(".text");
+    textEl.textContent = "Answer questions on Javascript fundamentals as quickly and accurately as possible. You have 60 seconds, for each incorrect answer a 10 second penalty will be applied. Click Start to begin";
+
+    var start = document.getElementById("start");
+    start.addEventListener("click", startQuiz);
+    var b1 = document.getElementById("b1");
+    var b2 = document.getElementById("b2");
+    var b3 = document.getElementById("b3");
+    var b4 = document.getElementById("b4");
+
+
 
 
 
